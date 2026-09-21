5b117499c2056da937a8c2a878ccc2e16fca8e9a
thewatermelonman <zwickdavid@yahoo.it>
2026-09-21 02:48:54 +0200

    add wrangler

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,19 +2,24 @@
 import { defineConfig, fontProviders } from 'astro/config';
 import react from "@astrojs/react";
 
+import cloudflare from '@astrojs/cloudflare';
+
 // https://astro.build/config
 export default defineConfig({
-	integrations: [react()],
-	fonts: [{
-		provider: fontProviders.local(),
-		name: "Nulshock",
-		cssVariable: "--font-nulshock",
-		options: {
-			variants: [{
-				src: ['./src/assets/fonts/Nulshock Bd.otf'],
-				weight: 'normal',
-				style: 'normal'
-			}]
-		}
+  integrations: [react()],
+
+  fonts: [{
+      provider: fontProviders.local(),
+      name: "Nulshock",
+      cssVariable: "--font-nulshock",
+      options: {
+          variants: [{
+              src: ['./src/assets/fonts/Nulshock Bd.otf'],
+              weight: 'normal',
+              style: 'normal'
+          }]
+      }
 	}],
+
+  adapter: cloudflare(),
 });
